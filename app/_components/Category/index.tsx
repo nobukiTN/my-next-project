3a9f3f19abc0bfd80e2530f1b7dec3a5aedad47f
mo ne <applebitebyboku@gmail.com>
2025-03-11 14:31:13 +0900

    5章まで完了

--- app/_components/Category/index.tsx
+++ app/_components/Category/index.tsx
@@ -1,11 +1,12 @@
-import type {Category} from "@/app/_libs/microcms";
-import styles from "./index.module.css";
+import type { Category } from '@/app/_libs/microcms';
+import styles from './index.module.css';
 
 type Props = {
-    category: Category;
+  category: Category;
 };
-
-export default function Category ({category} : Props){
-    return <span className={styles.tag}>{category.name} </span>;
+export default function Category({ category }: Props) {
+  if (!category) {
+    return <span className={styles.tag}>カテゴリなし</span>;
+  }
+  return <span className={styles.tag}>{category.name}</span>;
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,92 +1,39 @@
- import styles from "./page.module.css";
- import Image from "next/image";
+import styles from './page.module.css';
+import Image from 'next/image';
 
- import NewsList  from "@/app/_components//NewsList";
- import ButtonLink from "@/app/_components//ButtonLink";
-import {News} from "@/app/_libs/microcms";
+import { getNewsList } from '@/app/_libs/microcms';
+import { TOP_NEWS_LIMIT } from '@/app/_constants';
+import NewsList from '@/app/_components/NewsList';
+import ButtonLink from '@/app/_components/ButtonLink';
 
-const data: {
-  contents: News[] } ={
-    contents: [
-      {
-      id : "1",
-      title:"渋谷にオフィスを移しました",
-      category: {
-        name :"更新情報",
-      },
-      publishedAt : "2023/05/19",
-      createdAt : "2023/05/19",
-    },
-    {
-      id : "2",
-      title: "当社CEOが業界のリーダーTOP３０に選出されました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt:"2023/05/19",
-    },
-    {
-      id : "3",
-      title: "テストの記事です",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/04/19",
-      createdAt: "2023/04/19",
-    },
-    ],
-  };
-
-  export default  function Home() {
-    const sliceData = data.contents.slice(0,2);
-    return (
-      <>
-   <section className={styles.top}>
-   <div>
-   <h1 className={styles.title} >テクノロジーの力で世界を変える</h1>
-   <p className={styles.description} >私たちは市場をリードしているグローバルテックカンパニーです。</p> 
-   </div>
-   <Image className={styles.bgimg} src="/img-mv.jpg" alt="" width={4000} height={1200}/>
-   </section>
-
-      <section className={styles.news}>
-      <h2 className={styles.newsTitle}>News</h2>
-    <NewsList news={sliceData} />
-        {sliceData.map((article) => (
-          <li key={article.id} className={styles.list}>
-            <div className={styles.link}>
-              <Image 
-              className={styles.image}
-              src="/no-image.png"
-              alt="No Image"
-              width={1200}
-              height={630}
-            />
-            <dl className={styles.content}>
-              <dt className={styles.newsItemTitle}>{article.title}</dt>
-              <dd className={styles.meta}>
-                <span className={styles.tag}>{article.category.name}</span>
-                <span className={styles.date}>
-                  <Image 
-                  src="/clock.svg"
-                  alt=""
-                  width={16}
-                  height={16}
-                  priority
-                  />
-                  {article.publishedAt}
-                </span>
-              </dd>
-            </dl>
-            </div>
-          </li>
-        ))}
-
-      <div className={styles.newsLink}>
-        <ButtonLink href="/news">もっと見る</ButtonLink>
-      </div>
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
+  return (
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです。
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
       </section>
-      </>
-    );
-  }
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={data.contents} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
+      </section>
+    </>
+  );
+}
