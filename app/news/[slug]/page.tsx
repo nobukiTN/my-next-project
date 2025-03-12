16b64b661ba300102fb1f07dd44b128657689b03
mo ne <applebitebyboku@gmail.com>
2025-03-13 08:28:39 +0900

    ssr完了

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,27 +1,30 @@
-import { notFound } from "next/navigation";
-import { getNewsDetail } from "@/app/_libs/microcms";
-import Article from "@/app/_components/Article";
-import ButtonLink from "@/app/_components/ButtonLink";
-import styles from "./page.module.css";
+import { notFound } from 'next/navigation';
+import { getNewsDetail } from '@/app/_libs/microcms';
+import Article from '@/app/_components/Article';
+import ButtonLink from '@/app/_components/ButtonLink';
+import styles from './page.module.css';
 
 type Props = {
-    params: {
-        slug: string;
-    };
-    searchParams: {
-        dk?: string;
-    };
+  params: {
+    slug: string;
+  };
+  searchParams: {
+    dk?: string;
+  };
 };
 
-export default async function Page({params}: Props) {
-    const data = await getNewsDetail(params.slug).catch(notFound);
+export const revalidate = 0;
+export default async function Page({ params, searchParams }: Props) {
+  const data = await getNewsDetail(params.slug, {
+    draftKey: searchParams.dk,
+  }).catch(notFound);
 
-    return (
-        <>
-        <Article data={data} />
-        <div className={styles.footer}>
-            <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <Article data={data} />
+      <div className={styles.footer}>
+        <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
+      </div>
+    </>
+  );
 }
